d82f30df22c17cfa99087bdb45ea213aa2dbd30b
Tucker <tucker.cushing@kearsarge.org>
2018-10-02 07:45:36 -0400

    Halfway Through Story

--- story1.py
+++ story1.py
@@ -3,7 +3,7 @@ from getInput import *
 def playMadlibs():
     place1 = getWord("Enter a place: ")
     friend1 = getWord ("Enter a friend name: ")
-    numClowns = getNumber("Enter a number: ", 2, 20)
+    numClowns = getNumber("Enter a number: ", 2, 500)
     clowns1 = getWord("Enter a pluaral clown name: ")
     place2 = getWord ("Enter a place inside place1: ")
     
