92f3c7e98d171450d5d2f39660d22da85c5063b0
Tucker <tucker.cushing@kearsarge.org>
2018-10-03 07:41:01 -0400

    Start of class.

--- story1.py
+++ story1.py
@@ -6,15 +6,21 @@ def playMadlibs():
     numClowns = getNumber("Enter a number: ", 2, 500)
     clowns1 = getWord("Enter a pluaral clown name: ")
     place2 = getWord ("Enter a place inside place1: ")
+    object1 = getWord ("Enter an object: ")
+    verb1 = getWord ("Enter an verb ending in ing: ")
+    verb2 = getWord ("Enter a verb: ")
+    verb3 = getWord ("Enter a verb: ")
     
-    
+     
     output = ""
     output += "One day I was walking through " + place1
     output += ". Suddenly " + place1
     output += " started filling up with " + numClowns + " " + clowns1
     output += ". " + friend1
     output += " and I ran and hid in " + place2 + "" "."
-    
-    
-    
+    output += " all the " + clowns1 + " started running in our direction. "
+    output += friend1 + " pulled out a " + object1 + " and started " + verb1
+    output += ". " 
+    output += " the " " " + clowns1 + " " " broke into " " " + place2 + " " " and the " " " + object1 + " " "rendered useless."
+    output += "to escape I had to " " "+ verb2 + " " "out of" " "+ place2 + " " " and " " "+ verb3 + " " "to saftey" 
     return output
