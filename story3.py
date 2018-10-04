c028ba6661583cb7e8eb5740a2f2219eabb44ccc
John <john.davis@kearsarge.org>
2018-10-04 08:08:32 -0400

    s3

---
+++ story3.py
@@ -0,0 +1,23 @@
+from getInput import *
+
+def playMadlibs():
+    place1 = getWord ("Enter the place from previous story: ")
+    worker1 = getWord ("Enter a worker: ")
+    clown1 = getWord ("Enter a clown name: ")
+    clownarmy1 = getWord ("Enter a clown army name: ")
+    verb1 = getWord ("Enter a verb: ")
+    object1 = getWord ("Enter an object: ")
+    verb2 = getWord ("Enter a verb ending in ing: ")
+    worker2 = getWord ("Enter another worker name: ")
+    civilian1 = getWord ("Enter a civilian name: ")
+    
+       output = ""
+    output += 
+    output += 
+    output += 
+    output += 
+    output += 
+    output += 
+    output += 
+    output += 
+    return output
